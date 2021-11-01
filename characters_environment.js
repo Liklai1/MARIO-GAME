9568c302974eb819d4782250f60b2290d30b01b0
dracomalfoy13 <75836042+dracomalfoy13@users.noreply.github.com>
2021-11-01 16:22:04 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -55,9 +55,11 @@ var gameConfig={
 var noseX= "";
 var noseY= "";
 var gamestatus = "";
+
 function startGame(){
   gamestatus = "start";
   document.getElementById("status").innerHTML= "game is loading..";
+
 }
 function game(){
  console.log("noseX = " + noseX + ", noseY = " + noseY);
@@ -73,7 +75,7 @@ function game(){
     fill(255, 255, 255);
     textSize(40);
     textAlign(CENTER);
-    text("Press Any Arrow Keys to Start and Play ", gameConfig.screenX/2, gameConfig.screenY/2);
+    text("Press play button to start the game.", gameConfig.screenX/2, gameConfig.screenY/2);
     textSize(40);
 
     stroke(255);
@@ -121,7 +123,7 @@ function game(){
 
 // change game status if any key is pressed
 function changeGameStatud(character){
-  if((keyDown(control.up) ||keyDown(control.left)||keyDown(control.right) )&& gameConfig.status==="start") {
+  if(gamestatus == "start" && noseX != "" && gameConfig.status==="start") {
     world_start.play();
     initializeCharacterStatus(mario);
     gameConfig.status= "play";
@@ -285,13 +287,13 @@ function autoControl(character){
 function manualControl(character){
   
   if(character.live){
-    if(keyDown(control.left)){
+    if(noseX < 300){
       character.velocity.x-=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(-1);
     }
 
-    if(keyDown(control.right)){
+    if(noseX > 300){
       character.velocity.x+=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(1);
@@ -306,7 +308,7 @@ function manualControl(character){
 
 /* Movements of character */
 function jumping(character){
-	if( (keyWentDown(control.up)&&character.live) || (touchIsDown&&character.live) ){
+	if(( noseY < 200 && character.live) || (touchIsDown&&character.live) ){
 		character.velocity.y+=gameConfig.jump;
 	}
 }
